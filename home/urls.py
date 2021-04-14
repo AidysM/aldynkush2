b0bea46819ff9d6d35bd7982c1bce480cf85b8af
AidysM <mongush10011979@gmail.com>
2021-04-14 12:55:19 +0700

    created contact.html

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from .views import index, about_page
+from .views import index, about_page, contact_page, AKLoginView, profile, AKLogoutView
 
 app_name = 'home'
 
 urlpatterns = [
+    path('accounts/logout/', AKLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/login/', AKLoginView.as_view(), name='login'),
+    path('<str:page>/', contact_page, name='contact'),
     path('<str:page>/', about_page, name='about'),
-    path('', index, name='index')
+    path('', index, name='index'),
 ]
 
--- home/views.py
+++ home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     return render(request, 'home/index.html')
@@ -13,4 +16,20 @@ def about_page(request, page):
         raise Http404
     return HttpResponse(template.render(request=request))
 
+def contact_page(request, page):
+    try:
+        template = get_template('home/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+class AKLoginView(LoginView):
+    template_name = 'home/login.html'
+
+@login_required
+def profile(request):
+    return render(request, 'home/profile.html')
+
+class AKLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'home/logout.html'
 
